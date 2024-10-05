49c349e344f434791903d3a9bb03b3bafb89016f
Minh Quang Bui <minhquangbui053@gmail.com>
2024-10-05 10:11:31 +0700

    Add ncspot

--- setup_ubuntu_22.04.sh
+++ setup_ubuntu_22.04.sh
@@ -60,3 +60,34 @@ mkdir -p ~/.icons/Dracula
 sudo unzip Dracula.zip -d ~/.icons
 gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 sudo rm Dracula.zip
+
+# ncspot
+
+## install dependencies
+sudo apt-get install build-essential libasound2-dev
+sudo apt install libdbus-1-dev libncursesw5-dev libpulse-dev libssl-dev libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+
+## install ueberzug for rendering cover 
+echo 'deb http://download.opensuse.org/repositories/home:/justkidding/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:justkidding.list
+curl -fsSL https://download.opensuse.org/repositories/home:justkidding/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_justkidding.gpg > /dev/null
+sudo apt update
+sudo apt install ueberzugpp
+
+## build ncspot with cargo
+cd
+git clone https://github.com/hrkfdn/ncspot.git
+cd ncspot
+cargo build --features cover
+sudo cp target/debug/ncspot /usr/local/bin/ 
+
+## ncspot config
+cp -r ~/.config/nvim/.others/ncspot ~/.config/
+
+## use librespot to authenticate
+cd
+git clone https://github.com/librespot-org/librespot.git
+cd librespot 
+cargo build
+./target/debug/librespot --Cache ~/.cache/ncspot/ -j # cache the authencation
+mkdir ~/.cache/ncspot/librespot
+mv ~/.cache/ncspot/credentials.json ~/.cache/ncspot/librespot/
